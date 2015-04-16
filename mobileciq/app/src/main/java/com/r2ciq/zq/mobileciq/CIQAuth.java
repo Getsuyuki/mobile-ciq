b73297ce4d050fc15dc8ac5698756240cfb8a6fb
getsuyuki <li.zq.david@gmail.com>
2015-04-16 17:05:22 -0400

    patch for ciqauth skeleton

--- mobileciq/app/src/main/java/com/r2ciq/zq/mobileciq/CIQAuth.java
+++ mobileciq/app/src/main/java/com/r2ciq/zq/mobileciq/CIQAuth.java
@@ -13,12 +13,29 @@ public class CIQAuth extends Activity {
     private EditText passWordInput;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ciq_auth);
     }
 
-    public void submitCredentials (View view){
-       
+    public void submitCredentials (View view)
+    {
+        createAuthRequest();
+        sendAuthRequest();
+        //will not perform network requests before serverside is set up
+    }
+
+    //This can be altered in the future to build actual outgoing auth. request
+    public void createAuthRequest()
+    {
+
+    }
+
+    //1. before server is set up, it will do nothing
+    //2. after server is set up, it will perform http post via async while spinner loads
+    public void sendAuthRequest ()
+    {
+
     }
 }
